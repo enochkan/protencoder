92bb3f49b6c0e51e45d75d18d78b8eb56ee03017
Enoch Kan <kanxx030@gmail.com>
2019-02-15 21:37:26 -0600

    changed github repo in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pypairwisemap',
       version='0.4',
       description='Python utility for pairwise mapping of atomic spacing to represent protein structures in 2D',
-      url='https://github.com/enochk22/pdb-pairwise-mapping',
+      url='https://github.com/kanxx030/pdb-pairwise-mapping',
       author='Chi Nok Enoch Kan',
       author_email='kanxx030@gmail.com',
       license='MIT',
