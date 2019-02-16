fa1fc32a4cb1279d1a6ac55202016bd897f76d8a
Enoch Kan <kanxx030@gmail.com>
2019-02-15 19:10:54 -0600

    added plotting function and python packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='pypairwisemap',
+      version='0.2',
+      description='Python utility for pairwise mapping of atomic spacing to represent protein structures in 2D',
+      url='https://github.com/enochk22/pdb-pairwise-mapping',
+      author='Chi Nok Enoch Kan',
+      author_email='kanxx030@gmail.com',
+      license='MIT',
+      packages=['pypairwisemap'],
+      install_requires=[
+          'biopython','numpy','matplotlib'
+      ],
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Protein Engineering :: Bioinformatics',
+      ],
+      zip_safe=False)
